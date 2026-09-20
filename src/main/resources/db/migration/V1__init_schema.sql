4e8ae84ef4948eed109a523d00293659be509f23
gsyan <gsyan@naver.com>
2026-09-20 10:16:11 +0900

    전술력 수치 조정

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -63,7 +63,7 @@ CREATE TABLE commander (
     pvp_point_season_ref    INT             NOT NULL DEFAULT 0,
     name_change_count       INT             NOT NULL DEFAULT 2,
     command_power_max       INT             NOT NULL DEFAULT 120,
-    tactic_power_max         INT            NOT NULL DEFAULT 60,
+    tactic_power_max         INT            NOT NULL DEFAULT 1000,
     exploration_point           INT         NOT NULL DEFAULT 0,
     exploration_point_earned_total INT      NOT NULL DEFAULT 0,
     achievement_point           INT         NOT NULL DEFAULT 0,
